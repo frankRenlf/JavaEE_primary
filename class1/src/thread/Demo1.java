c733669fc0acdb3e1af11593ac488e98f9018ec9
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-30 11:29:23 +0800

    Create Demo1.java

---
+++ class1/src/thread/Demo1.java
@@ -0,0 +1,18 @@
+package thread;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : class1
+ * @Package : thread
+ * @createTime : 2022/7/30 11:29
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Demo1 {
+
+    
+}
