d11dbcb2c8f24b0faf445ab3c839ff01c59975a9
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-30 12:25:27 +0800

    update sleep test

--- class1/src/thread/Demo1.java
+++ class1/src/thread/Demo1.java
@@ -12,7 +12,25 @@ package thread;
  * @github : https://github.com/frankRenlf
  * @Description :
  */
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Hello thread!");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
 public class Demo1 {
 
-    
+    public static void main(String[] args) {
+        Thread myThread = new MyThread();
+        myThread.start();
+        System.out.println(123);
+    }
+
 }
