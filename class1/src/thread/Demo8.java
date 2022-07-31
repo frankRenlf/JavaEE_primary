df5b831a03a30b9f3668da39e10bc55dd454876a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-31 11:28:53 +0800

    create demo 10

--- class1/src/thread/Demo8.java
+++ class1/src/thread/Demo8.java
@@ -15,7 +15,23 @@ package thread;
 public class Demo8 {
 
     public static void main(String[] args) {
-        
+        Thread t = new Thread(()->{
+            while(true){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        },"frank");
+        t.start();
+        System.out.println(t.getId());
+        System.out.println(t.getName());
+        System.out.println(t.getState());
+        System.out.println(t.isInterrupted());
+        System.out.println(t.getPriority());
+        System.out.println(t.isAlive());
     }
+
 
 }
---
+++ class1/src/thread/demo10.java
@@ -0,0 +1,16 @@
+package thread;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : class1
+ * @Package : thread
+ * @createTime : 2022/7/31 11:28
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class demo10 {
+}
