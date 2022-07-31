54dc55cbb78a4d605f2e6160b450768cfd7a402c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-31 11:01:03 +0800

    add set deamon test

--- class1/src/thread/Demo7.java
+++ class1/src/thread/Demo7.java
@@ -14,18 +14,66 @@ package thread;
  */
 public class Demo7 {
 
-    public static void main1(String[] args) {
+    public static void main(String[] args) {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("hello");
+                while(true){
+                    System.out.println("hello");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         }, "thread1");
+        t1.setDaemon(true);
         t1.start();
     }
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
+        Thread[] threads = new Thread[20];
+        for (int i = 0; i < threads.length; i++) {
+            final int n = i;
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(n);
+                }
+            });
+        }
+        for (Thread t : threads) {
+            t.start();
+        }
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("ok");
+    }
 
+    public static void main3(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[20];
+        for(int i=0; i<20; i++){
+            final int n = i;
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {//内部类使用外部的变量，必须是final修饰
+                    System.out.println(n);
+                }
+            });
+        }
+        for(Thread t : threads){
+            t.start();
+        }
+        for(Thread t : threads){//同时执行20个线程，再等待所有线程执行完毕
+            t.join();
+        }
+        System.out.println("OK");
     }
 
 }
