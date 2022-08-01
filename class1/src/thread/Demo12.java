9795be5b758198747c649708bb637b50be143d97
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-01 12:39:24 +0800

    update

--- class1/src/thread/Demo12.java
+++ class1/src/thread/Demo12.java
@@ -13,4 +13,9 @@ package thread;
  * @Description :
  */
 public class Demo12 {
+    public static void main(String[] args) {
+        for (Thread.State state : Thread.State.values()) {
+            System.out.println(state);
+        }
+    }
 }
