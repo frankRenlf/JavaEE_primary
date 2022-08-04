f212be8fcbaee597f7af95a3513d100dcd3025e9
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-04 14:13:33 +0800

    update singleton

--- class1/src/thread/Demo20.java
+++ class1/src/thread/Demo20.java
@@ -1,5 +1,7 @@
 package thread;
 
+import java.lang.reflect.Array;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -7,34 +9,85 @@ package thread;
  * @version : 1.0
  * @Project : class1
  * @Package : thread
- * @createTime : 2022/8/3 15:15
+ * @createTime : 2022/8/4 12:03
  * @Email : sc19lr@leeds.ac.uk
  * @github : https://github.com/frankRenlf
  * @Description :
  */
 
-class Singleton {
+class MyBlockQueue<T> {
+    private T[] item;
+    private int head = 0;
+    private int tail = 0;
+    private volatile int size = 0;
 
-    private volatile static Singleton instance = null;
 
-    public static Singleton getInstance() {
-        if (instance == null) {
-            synchronized (Singleton.class) {
-                if (instance == null) {
-                    instance = new Singleton();
-                }
+    public MyBlockQueue(Class<T> clazz, int length) {
+        item = (T[]) Array.newInstance(clazz, length);
+    }
+
+    public void put(T val) throws InterruptedException {
+        synchronized (this) {
+            while (size == item.length) {
+                this.wait();
             }
+            item[tail] = val;
+            tail++;
+            if (tail == item.length) {
+                tail = 0;
+            }
+            size++;
+            this.notify();
         }
-        return instance;
     }
 
-    private Singleton() {
+    public T take() throws InterruptedException {
+        synchronized (this) {
+            while (size == 0) {
+                this.wait();
+            }
+            T ret = item[head];
+            head++;
+            if (head == item.length) {
+                head = 0;
+            }
+            size--;
+            this.notify();
+            return ret;
+        }
     }
+
 
 }
 
-
 public class Demo20 {
 
+    public static void main(String[] args) throws InterruptedException {
+        MyBlockQueue<Integer> myBlockQueue = new MyBlockQueue<>(Integer.class, 5);
+        Thread t1 = new Thread(() -> {
+            try {
+                int index = 0;
+                while (true) {
+                    System.out.println("put: " + index);
+                    myBlockQueue.put(index);
+                    index++;
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            try {
+                while (true) {
+                    System.out.println("take: " + myBlockQueue.take());
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        t1.start();
+        t2.start();
+    }
 
 }
---
+++ class1/src/thread/Singleton.java
@@ -0,0 +1,36 @@
+package thread;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : class1
+ * @Package : thread
+ * @createTime : 2022/8/3 15:15
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+
+public class Singleton {
+
+    private volatile static Singleton instance = null;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private Singleton() {
+    }
+
+}
+
+
