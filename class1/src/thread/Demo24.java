20bd4349721f0f959f145b333cf1a548bc9d8866
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-05 14:25:24 +0800

    Demo25

---
+++ class1/src/thread/Demo24.java
@@ -0,0 +1,27 @@
+package thread;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : class1
+ * @Package : thread
+ * @createTime : 2022/8/5 14:05
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Demo24 {
+
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+    }
+
+}
---
+++ class1/src/thread/Demo25.java
@@ -0,0 +1,16 @@
+package thread;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : class1
+ * @Package : thread
+ * @createTime : 2022/8/5 14:25
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Demo25 {
+}
