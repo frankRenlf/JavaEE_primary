4dc081aa16dc17c565f2c07b5c238b8cea77f17c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-05 14:45:45 +0800

    modify thread pool

--- class1/src/thread/Demo25.java
+++ class1/src/thread/Demo25.java
@@ -18,13 +18,48 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 class MyThreadPool {
 
-    private BlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
+    private BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
 
     public void submit(Runnable runnable) {
+        try {
+            queue.put(runnable);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public MyThreadPool(int n) {
+        for (int i = 0; i < n; i++) {
+            Thread t = new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        Runnable runnable = queue.take();
+                        runnable.run();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+                }
+            });
+            t.start();
+        }
     }
 
 }
 
 public class Demo25 {
+
+    public static void main(String[] args) {
+        MyThreadPool myThreadPool = new MyThreadPool(10);
+        for (int i = 0; i < 100; i++) {
+            int finalI = i;
+            myThreadPool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(finalI);
+                }
+            });
+        }
+    }
+
 }
