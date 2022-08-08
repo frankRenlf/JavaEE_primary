a05fd55021c79ab0732195127d482f50cbd9219b
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-08 15:03:11 +0800

    modify file operation

---
+++ IO_operation/src/file/Demo6.java
@@ -0,0 +1,32 @@
+package file;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : IO_operation
+ * @Package : file
+ * @createTime : 2022/8/8 14:40
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Demo6 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream is = new FileInputStream("./src/files/change.txt");
+        Scanner sc = new Scanner(is, StandardCharsets.UTF_8);
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+    }
+
+}
---
+++ IO_operation/src/file/Demo7.java
@@ -0,0 +1,30 @@
+package file;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : IO_operation
+ * @Package : file
+ * @createTime : 2022/8/8 14:55
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Demo7 {
+
+    public static void main(String[] args) throws IOException {
+        OutputStream os = new FileOutputStream("./src/files/change.txt");
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+        PrintWriter pw = new PrintWriter(outputStreamWriter);
+        pw.println("123");
+        pw.print("frank");
+        pw.print("second");
+        pw.flush();
+    }
+
+}
