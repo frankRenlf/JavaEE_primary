9c679b935c2adc5712728e6b1e7d7024ab2d438b
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-12 20:02:24 +0800

    update

--- FrontEnd/src/resources/static/js/test1.js
+++ FrontEnd/src/resources/static/js/test1.js
@@ -1,1 +1,9 @@
-alert("hehe");
+var name = prompt("请输入姓名:");
+var age = prompt("请输入年龄:");
+var score = prompt("请输入分数");
+alert("您的姓名是: " + name);
+alert("您的年龄是: " + age);
+alert("您的分数是: " + score);
+console.log(name);
+console.log(age);
+console.log(score);
