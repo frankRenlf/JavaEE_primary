4be264200885f282b9305c7d08d9cac8f0ed199a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-12 21:11:34 +0800

    update

--- FrontEnd/src/resources/static/js/test1.js
+++ FrontEnd/src/resources/static/js/test1.js
@@ -1,9 +1,19 @@
-var name = prompt("请输入姓名:");
-var age = prompt("请输入年龄:");
-var score = prompt("请输入分数");
-alert("您的姓名是: " + name);
-alert("您的年龄是: " + age);
-alert("您的分数是: " + score);
-console.log(name);
-console.log(age);
-console.log(score);
+// var name = prompt("请输入姓名:");
+// var age = prompt("请输入年龄:");
+// var score = prompt("请输入分数");
+// alert("您的姓名是: " + name);
+// alert("您的年龄是: " + age);
+// alert("您的分数是: " + score);
+// console.log(name);
+// console.log(age);
+// console.log(score);
+// var elems = document.querySelectorAll("div");
+// console.log(elems);
+
+var btn = document.getElementById("btn");
+btn.onclick = function () {
+  alert("hello world");
+};
+console.log(btn);
+var img = document.querySelector("img");
+console.dir(img);
