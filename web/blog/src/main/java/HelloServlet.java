121993104251c23ffdf4a55ae1a6333eddf89eee
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-29 10:32:17 +0800

    update

---
+++ web/blog/src/main/java/HelloServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : web
+ * @Package : PACKAGE_NAME
+ * @createTime : 2022/9/29 10:30
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        System.out.println("hello");
+        resp.getWriter().write("hello");
+    }
+}
