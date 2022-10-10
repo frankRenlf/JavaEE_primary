24fbff7731bc07b439a093366272e1714f4ac811
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-10 15:40:47 +0800

    update

---
+++ servlet/class2/src/main/java/TestHello.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : servlet
+ * @Package : PACKAGE_NAME
+ * @createTime : 2022/10/10 15:38
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class TestHello extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
