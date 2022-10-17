2c559e81cf7e327d75d3eee524dae2cb4bffa947
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-17 20:41:03 +0800

    initiate dao model

---
+++ servlet/blog_demo/src/main/java/dao/BlogDao.java
@@ -0,0 +1,38 @@
+package dao;
+
+import model.Blog;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : servlet
+ * @Package : dao
+ * @createTime : 2022/10/17 20:37
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class BlogDao {
+
+    public void insert(Blog blog){
+
+    }
+
+    public List<Blog> selectAll(){
+
+    }
+
+    public Blog getById(Integer id){
+
+    }
+
+    public void deleteById(Integer id){
+
+    }
+
+
+}
---
+++ servlet/blog_demo/src/main/java/dao/UserDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : servlet
+ * @Package : dao
+ * @createTime : 2022/10/17 20:37
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class UserDao {
+}
